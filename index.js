993a1c77f7e62adcf8e3ab82e09b7febabc1aea7
Eddie Xu <wxu55@my.bcit.ca>
2023-04-30 20:12:38 -0700

    pushing

--- index.js
+++ index.js
@@ -32,6 +32,29 @@ var {database} = include('databaseConnection');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://EddieQVQ:<password>@cluster.7kuiygn.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir)
+
 const userCollection = database.db(mongodb_database).collection('users');
 
 app.use(express.urlencoded({extended: false}));
