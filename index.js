6d53d28dfba76d8392d0452c37708134706184dd
Eddie Xu <wxu55@my.bcit.ca>
2023-04-30 21:19:17 -0700

    2

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
